5d75e197aa50c3b90062508fe9b3f962387cbd35
AlexBrandt46 <ajb3155@gmail.com>
2017-08-31 21:04:46 -0400

    Created the signature for the search method, the constructor, and the instance variables.

--- Hangman/Hangman.cs
+++ Hangman/Hangman.cs
@@ -5,6 +5,27 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Hangman {
+
     class Hangman {
+
+        private int bodyCounter; //counts how many bosy parts there currently are
+        private char[] letters; //contains each individual letter of the sentence entered by the user
+        private char[] guesses; //the string representation of the correct guesses
+        private List<char> wrong; //the wrong guesses
+
+        public Hangman(string sentence) {
+
+            bodyCounter = 0;
+            letters = sentence.ToCharArray();
+            guesses = new char[letters.Length];
+            wrong = new List<char>();
+
+        }
+
+        public void Search(char letter) {
+
+        }
+
     }
+
 }
